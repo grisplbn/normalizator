88d84c62ff377dd7031ffd68379314a457634177
Grisp <pioluc@gmail.com>
2025-12-10 12:06:35 +0100

    Refactor SQL query in dbquery.sql to use new table structure and improve filtering conditions for city, street name, and postal code.

--- dbquery.sql
+++ dbquery.sql
@@ -1,17 +1,18 @@
 SELECT
-    street_prefix  AS "StreetPrefix",
-    street_name    AS "StreetName",
-    building_no    AS "BuildingNumber",
-    city           AS "City",
-    postal_code    AS "PostalCode",
-    commune        AS "Commune",
-    district       AS "District",
-    province       AS "Province"
-FROM addresses
+    NULL                AS "StreetPrefix",
+    s.name              AS "StreetName",
+    NULL                AS "BuildingNumber",
+    c.name              AS "City",
+    pc.code             AS "PostalCode",
+    NULL                AS "Commune",
+    NULL                AS "District",
+    NULL                AS "Province"
+FROM "primary".tb_address_property_mappings map
+LEFT JOIN "primary".tb_cities c        ON c.id = map.city_id
+LEFT JOIN "primary".tb_streets s       ON s.id = map.street_id
+LEFT JOIN "primary".tb_postal_codes pc ON pc.id = map.postal_code_id
 WHERE
-    street_name ILIKE @StreetName
-    AND COALESCE(street_prefix, '') ILIKE @StreetPrefix
-    AND COALESCE(building_no, '') = @BuildingNumber
-    AND COALESCE(city, '') ILIKE @City
-    AND COALESCE(postal_code, '') = @PostalCode;
+    ( @City        = '' OR c.name ILIKE '%' || @City        || '%' )
+    AND ( @StreetName   = '' OR s.name ILIKE '%' || @StreetName   || '%' )
+    AND ( @PostalCode   = '' OR pc.code ILIKE '%' || @PostalCode  || '%' );
 
